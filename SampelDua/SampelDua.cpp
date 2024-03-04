4927d086cbbd5170fd71d8644037b2e8f49522d4
bloodfunny <adhyaakmalrafiansyah@email.com>
2024-03-04 11:57:28 +0700

    Add project files.

---
+++ SampelDua/SampelDua.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	srand(time(0));
+
+	int nBilA, nBilB;
+
+	string status;
+
+	nBilA = rand() % 11;
+	nBilB = rand() % 11;
+
+	if (nBilA)
+
+
+}
+
